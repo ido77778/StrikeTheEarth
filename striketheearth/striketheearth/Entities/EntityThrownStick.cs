c05801b668d3fdad62ab488b5b99a3b3e7fb2922
hayyerboa <155395003+hayyerboa@users.noreply.github.com>
2025-04-03 04:17:28 +0300

    Add project files.

---
+++ striketheearth/striketheearth/Entities/EntityThrownStick.cs
@@ -0,0 +1,152 @@
+﻿using System.IO;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+
+namespace StrikeTheEarth.Entities
+{
+	internal class EntityThrownStick : Entity
+	{
+		bool beforeCollided;
+		bool stuck;
+		long msLaunch;
+		Vec3d motionBeforeCollide = new Vec3d();
+
+		public Entity FiredBy;
+		public float Damage;
+		public ItemStack ProjectileStack;
+
+		public override bool IsInteractable
+		{
+			get { return false; }
+		}
+
+		public override void Initialize(EntityProperties properties, ICoreAPI api, long InChunkIndex3d)
+		{
+			base.Initialize(properties, api, InChunkIndex3d);
+
+			msLaunch = World.ElapsedMilliseconds;
+
+			if (ProjectileStack?.Collectible != null)
+			{
+				ProjectileStack.ResolveBlockOrItem(World);
+			}
+		}
+
+		public override void OnGameTick(float dt)
+		{
+			base.OnGameTick(dt);
+			if (ShouldDespawn) return;
+
+			EntityPos pos = SidedPos;
+
+			stuck = Collided;
+			pos.Pitch = GameMath.PIHALF;
+			pos.Roll = 0;
+			pos.Yaw = GameMath.PIHALF;
+
+			if (stuck)
+			{
+				if (!beforeCollided && World.Side == EnumAppSide.Server)
+				{
+					float strength = GameMath.Clamp((float)motionBeforeCollide.Length() * 4, 0, 1);
+
+					if (CollidedHorizontally || CollidedVertically)
+					{
+						OnImpact();
+						return;
+					}
+
+					// Resend position to client
+					WatchedAttributes.MarkAllDirty();
+				}
+
+				beforeCollided = true;
+				return;
+			}
+
+
+			if (Damage > 0 && World.Side == EnumAppSide.Server)
+			{
+				Entity entity = World.GetNearestEntity(ServerPos.XYZ, 5f, 5f, (e) => {
+					if (e.EntityId == this.EntityId || (FiredBy != null && e.EntityId == FiredBy.EntityId && World.ElapsedMilliseconds - msLaunch < 500) || !e.IsInteractable)
+					{
+						return false;
+					}
+
+					double dist = e.SelectionBox.ToDouble().Translate(e.ServerPos.X, e.ServerPos.Y, e.ServerPos.Z).ShortestDistanceFrom(ServerPos.X, ServerPos.Y, ServerPos.Z);
+					return dist < 0.5f;
+				});
+
+				if (entity != null)
+				{
+					entity.ReceiveDamage(new DamageSource
+					{
+						Source = EnumDamageSource.Entity,
+						SourceEntity = this,
+						CauseEntity = FiredBy,
+						Type = EnumDamageType.BluntAttack
+					}, Damage);
+					OnImpact();
+					return;
+				}
+			}
+
+			beforeCollided = false;
+			motionBeforeCollide.Set(pos.Motion.X, pos.Motion.Y, pos.Motion.Z);
+
+		}
+
+
+		public void OnImpact()
+		{
+			World.PlaySoundAt(new AssetLocation("sounds/effect/toolbreak"), this, null, false, 32);
+			World.SpawnCubeParticles(SidedPos.XYZ.OffsetCopy(0, 0.2, 0), ProjectileStack, 0.8f, 20);
+			Die();
+
+			EntityProperties type = World.GetEntityType(new AssetLocation("beemob"));
+			Entity entity = World.ClassRegistry.CreateEntity(type);
+
+			if (entity != null)
+			{
+				entity.ServerPos.X = SidedPos.X + 0.5f;
+				entity.ServerPos.Y = SidedPos.Y + 0.5f;
+				entity.ServerPos.Z = SidedPos.Z + 0.5f;
+				entity.ServerPos.Yaw = (float)World.Rand.NextDouble() * 2 * GameMath.PI;
+				entity.Pos.SetFrom(entity.ServerPos);
+
+				entity.Attributes.SetString("origin", "brokenbeenade");
+				World.SpawnEntity(entity);
+			}
+		}
+
+
+		public override bool CanCollect(Entity byEntity)
+		{
+			return false;
+		}
+
+		public override void OnCollideWithLiquid()
+		{
+			base.OnCollideWithLiquid();
+		}
+
+		public override void ToBytes(BinaryWriter writer, bool forClient)
+		{
+			base.ToBytes(writer, forClient);
+			writer.Write(beforeCollided);
+			ProjectileStack.ToBytes(writer);
+		}
+
+		public override void FromBytes(BinaryReader reader, bool fromServer)
+		{
+			base.FromBytes(reader, fromServer);
+			beforeCollided = reader.ReadBoolean();
+
+			ProjectileStack = World == null ? new ItemStack(reader) : new ItemStack(reader, World);
+		}
+	}
+}
---
+++ striketheearth/striketheearth/StrikeTheEarthModSystem.cs
@@ -0,0 +1,31 @@
+﻿using StrikeTheEarth.Entities;
+using StrikeTheEarth.Items;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace StrikeTheEarth
+{
+    public class StrikeTheEarthModSystem : ModSystem
+    {
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod: " + api.Side);
+            api.RegisterItemClass(Mod.Info.ModID + ".throwingstick", typeof(ItemThrowingStick));
+            api.RegisterEntity(Mod.Info.ModID + ".thrownstick", typeof(EntityThrownStick));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod server side: " + Lang.Get("striketheearth:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            Mod.Logger.Notification("Hello from template mod client side: " + Lang.Get("striketheearth:hello"));
+        }
+    }
+}
